0d818b417903002bd7fcf2fe6111d39539392ad7
Liova <liovaswagg@hotmail.com>
2023-01-26 15:11:37 +0100

    Classe_fini

--- Client_parking/client_parking.h
+++ Client_parking/client_parking.h
@@ -1,13 +1,25 @@
 #ifndef CLIENT_PARKING_H
 #define CLIENT_PARKING_H
 
+#include <iostream>
+using namespace std;
 
 class Client_parking
 {
 private:
+    string _nom;
+    string _prenom;
 
 public:
     Client_parking();
+    Client_parking(string nom, string prenom);
+    ~Client_parking();
+    string getNom();
+    void setNom(string nom);
+    string getPrenom();
+    void setPrenom(string prenom);
+    void afficherInfoClientParking();
+
 };
 
 #endif // CLIENT_PARKING_H
