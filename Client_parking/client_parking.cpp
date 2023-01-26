6c8b46a11fddaa64bcfd376e40b3070c4d6f130e
Liova <liovaswagg@hotmail.com>
2023-01-26 17:00:31 +0100

    modif

--- Client_parking/client_parking.cpp
+++ Client_parking/client_parking.cpp
@@ -7,8 +7,17 @@
  */
 
 #include "client_parking.h"
+#include <iostream>
+
+using namespace std;
 
 Client_parking::Client_parking()
-{
 
-}
+    : _nom("Nom")
+    , _prenom("Prenom")
+
+Client_parking::~Client_parking();
+
+
+
+
